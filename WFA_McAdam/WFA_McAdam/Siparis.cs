a2eadaf360f6c4585fe49a97e2586c5d1ee7ea04
umut.yilbas <umut.yilbas@windowslive.com>
2022-04-07 11:44:36 +0300

    Ekstra olmadığında alınan hata giderildi.

--- WFA_McAdam/WFA_McAdam/Siparis.cs
+++ WFA_McAdam/WFA_McAdam/Siparis.cs
@@ -10,30 +10,43 @@ namespace WFA_McAdam
     {
         public Menu Menu { get; set; }
         private decimal _fiyat = 0;
-        public decimal Fiyat { get
-            {  _fiyat = Menu.Fiyat;
+        public decimal Fiyat
+        {
+            get
+            {
+                _fiyat = Menu.Fiyat;
                 foreach (Ekstra ekstra in Ekstralar)
                 {
                     _fiyat += ekstra.Fiyat;
 
                 }
-                
+
                 _fiyat *= Adet;
                 return _fiyat;
-                
-            }  }
+
+            }
+        }
         public List<Ekstra> Ekstralar { get; set; }
         public byte Adet { get; set; }
         public override string ToString()
         {
             string format = "";
-            foreach (Ekstra ekstra in Ekstralar)
-            {
-                format += ekstra;
 
+            if (Ekstralar.Count < 1)
+            {
+                return Menu.Ad + " Sipariş Adedi: " + Adet + " Toplam Fiyat: " + Fiyat;
+            }
+            else
+            {
+                foreach (Ekstra ekstra in Ekstralar)
+                {
+                    format += ekstra;
+
+                }
+                return Menu.Ad+" Menü " + format + " Sipariş Adedi: " + Adet + " Toplam Fiyat: " + Fiyat;
             }
 
-            return Menu.Ad + " " + format + " Sipariş Adedi: " + Adet + " Toplam Fiyat: " + Fiyat;
+
         }
 
 
@@ -41,3 +54,6 @@ namespace WFA_McAdam
 
     }
 }
+
+
+
